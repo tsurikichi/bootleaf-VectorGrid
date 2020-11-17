d7c6ed56d0415a9ab04f44e59349877f4ff816fb
yuu-matsushita <h2.yuu.mat@gmail.com>
2020-11-18 01:22:51 +0900

    urlをgitに修正

--- service_worker.js
+++ service_worker.js
@@ -1,7 +1,7 @@
 // キャッシュファイルの指定
 var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
-    '/poster-keisuke.github.io/',
+    '/tsurikichi.github.io/leaflet-VectorGrid/index.html',
 ];
 
 // インストール処理
